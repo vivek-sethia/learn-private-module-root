aa33ad118e7fb80c71965202a0f39c76e1cfba00
vivek-sethia <sethiavivek2006@gmail.com>
2021-01-15 11:13:35 +0100

    changeS

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "s3-webapp" {
-  source  = "app.terraform.io/hashicorp-learn/s3-webapp/aws"
+  source  = "app.terraform.io/certprep/demo-s3-app-module/awss"
   name        = var.name
   region = var.region
   prefix = var.prefix
